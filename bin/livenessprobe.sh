dde76632819d23852e35c7c2ec3482a48fa7a0e2
Abdellatif BOUCHAMA <abdellatif.bouchama@gmail.com>
2020-04-23 15:38:23 +0200

    Update livenessprobe.sh

--- bin/livenessprobe.sh
+++ bin/livenessprobe.sh
@@ -11,26 +11,13 @@ echo "producer executed" >> "${LOG}"
 /opt/amq/bin/activemq consumer --user admin --password admin --brokerUrl tcp://$HOSTNAME:61616 --destination livenessprobeq --messageCount 1
 echo "consumer executed" >> "${LOG}"
 
+CONNECT_RESULT=$?
+echo ${CONNECT_RESULT} >> "${LOG}"
 
-errorCode=$?
-if [ $errorCode -ne 0 ]; then
-  echo "exit0" >> "${LOG}"
+if [ "${CONNECT_RESULT}" -eq 0 ] ; then
+        echo "exit0" >> "${LOG}"
         exit 0;
     else
         echo "exit1" >> "${LOG}"
         exit 1;
 fi
-
-#/opt/amq/bin/activemq producer --user admin --password admin --brokerUrl tcp://$HOSTNAME:61616 --destination livenessprobeq --messageCount 1 --msgTTL 60000 --persistent false
-#/opt/amq/bin/activemq consumer --user admin --password admin --brokerUrl tcp://$HOSTNAME:61616 --destination livenessprobeq --messageCount 1
-
-#CONNECT_RESULT=$?
-#echo ${CONNECT_RESULT} >> "${LOG}"
-
-#if [ "${CONNECT_RESULT}" -eq 0 ] ; then
-#        echo "exit0" >> "${LOG}"
-#        exit 0;
-#    else
-#        echo "exit1" >> "${LOG}"
-#        exit 1;
-#fi
