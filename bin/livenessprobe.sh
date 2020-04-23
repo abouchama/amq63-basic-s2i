d9cef46c266bd7ac93a523739d97ab9994fc249b
Abdellatif BOUCHAMA <abdellatif.bouchama@gmail.com>
2020-04-23 10:53:38 +0200

    Update livenessprobe.sh

--- bin/livenessprobe.sh
+++ bin/livenessprobe.sh
@@ -9,7 +9,8 @@ echo " --- Liveness Probe Started --- " > "${LOG}"
 #while : ; do
 #CONNECT_RESULT=1
 
-/opt/amq/bin/activemq producer --user admin --password admin --brokerUrl tcp://$HOSTNAME:61616 --destination livenessprobeq --messageCount 1 --msgTTL 60000 --persistent false && /opt/amq/bin/activemq consumer --user admin --password admin --brokerUrl tcp://$HOSTNAME:61616 --destination livenessprobeq --messageCount 1
+/opt/amq/bin/activemq producer --user admin --password admin --brokerUrl tcp://$HOSTNAME:61616 --destination livenessprobeq --messageCount 1 --msgTTL 60000 --persistent false
+/opt/amq/bin/activemq consumer --user admin --password admin --brokerUrl tcp://$HOSTNAME:61616 --destination livenessprobeq --messageCount 1
 
 #source /opt/amq/bin/activemq producer --user admin --password admin --brokerUrl tcp://$HOSTNAME:61616 --destination livenessprobeq --messageCount 1 --msgTTL 60000 --persistent false
 #source /opt/amq/bin/activemq consumer --user admin --password admin --brokerUrl tcp://$HOSTNAME:61616 --destination livenessprobeq --messageCount 1
