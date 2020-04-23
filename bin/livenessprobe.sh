d19f2c4bc1384853e3573377701feb976f08fccf
Abdellatif BOUCHAMA <abdellatif.bouchama@gmail.com>
2020-04-23 10:07:31 +0200

    Update livenessprobe.sh

--- bin/livenessprobe.sh
+++ bin/livenessprobe.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-#CONNECT_RESULT=1
+OUTPUT=/tmp/liveness-output
+ERROR=/tmp/liveness-error
+LOG=/tmp/liveness-log
 
-/opt/amq/bin/activemq producer --user admin --password admin --brokerUrl tcp://$HOSTNAME:61616 --destination livenessprobeq --messageCount 1 --msgTTL 60000 --persistent false && /opt/amq/bin/activemq consumer --user admin --password admin --brokerUrl tcp://$HOSTNAME:61616 --destination livenessprobeq --messageCount 1
+echo "--- Liveness Probe Started --- " > "${LOG}"
+
+#while : ; do
+CONNECT_RESULT=1
+
+source /opt/amq/bin/activemq producer --user amq --password topSecret --brokerUrl tcp://$HOSTNAME:61616 --destination livenessprobeq --messageCount 1 --msgTTL 60000 --persistent false
+source /opt/amq/bin/activemq consumer --user amq --password topSecret --brokerUrl tcp://$HOSTNAME:61616 --destination livenessprobeq --messageCount 1
 
 CONNECT_RESULT=$?
+echo ${CONNECT_RESULT} > "${LOG}"
 
 if [ "${CONNECT_RESULT}" -eq 0 ] ; then
+        echo "exit0" > "${LOG}"
         exit 0;
     else
         exit 1;
-    fi
+fi
