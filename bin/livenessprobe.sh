da21706d180f84c1b3bf14aaf6516eb8f9fcde97
Abdellatif BOUCHAMA <abdellatif.bouchama@gmail.com>
2020-04-23 16:25:55 +0200

    Update livenessprobe.sh

--- bin/livenessprobe.sh
+++ bin/livenessprobe.sh
@@ -4,8 +4,13 @@ LOG=/tmp/liveness-log
 
 echo " --- Liveness Probe Started --- " > "${LOG}"
 
-/opt/amq/bin/activemq producer --user admin --password admin --brokerUrl tcp://$HOSTNAME:61616 --destination livenessprobeq --messageCount 1 --msgTTL 60000 --persistent false
+if /opt/amq/bin/activemq producer --user admin --password admin --brokerUrl tcp://$HOSTNAME:61616 --destination livenessprobeq --messageCount 1 --msgTTL 60000 --persistent false; then
 echo "producer executed" >> "${LOG}"
+else
+        echo "exit1" >> "${LOG}"
+        exit 1;
+fi
+
 /opt/amq/bin/activemq consumer --user admin --password admin --brokerUrl tcp://$HOSTNAME:61616 --destination livenessprobeq --messageCount 1
 echo "consumer executed" >> "${LOG}"
 
